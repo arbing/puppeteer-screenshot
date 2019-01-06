0605f4625da98a6ba89db1ebccefafc1050f20e2
ArBing <xiebingting@gmail.com>
2019-01-07 02:20:01 +0800

    升级nodemon

--- README.md
+++ README.md
@@ -1,40 +1,39 @@
 # sreenshot
 
+###
 
+#### GET / 可[参考][1]
 
-### 
-
-#### GET /   可[参考][1]
-| arguments  | default  | required  | desc  |
-|---|---|---|---|
-|  url | -  | true  | sreenshot webpage url  |
-|  type | png  | false  | image type  |
-| quality   | -  | false  | 图片质量, 可选值 0-100. png 类型不适用  |
-| fullPage  | true  | false  | 对完整的页面截图  |
-| x  | 0  | false  | 裁剪区域相对于左上角（0， 0）的x坐标  |
-| y  | 0  | false  |  裁剪区域相对于左上角（0， 0）的y坐标  |
-| w  |  200 |  false |  裁剪的宽度 |
-| h  |  200 |  false |  裁剪的高度 |
-| o  |  false | false  | 隐藏默认的白色背景，背景透明  |
-| device  | iPhone 8  | false  | 模拟设备，可选值[参考][2]  |
-|   |   |   |   |
+| arguments | default  | required | desc                                   |
+| --------- | -------- | -------- | -------------------------------------- |
+| url       | -        | true     | sreenshot webpage url                  |
+| type      | png      | false    | image type                             |
+| quality   | -        | false    | 图片质量, 可选值 0-100. png 类型不适用 |
+| fullPage  | true     | false    | 对完整的页面截图                       |
+| x         | 0        | false    | 裁剪区域相对于左上角（0， 0）的 x 坐标 |
+| y         | 0        | false    | 裁剪区域相对于左上角（0， 0）的 y 坐标 |
+| w         | 200      | false    | 裁剪的宽度                             |
+| h         | 200      | false    | 裁剪的高度                             |
+| o         | false    | false    | 隐藏默认的白色背景，背景透明           |
+| device    | iPhone 8 | false    | 模拟设备，可选值[参考][2]              |
+|           |          |          |                                        |
 
 ### POST /
-| arguments  | type | default  | required  | desc  |
-|---|---|---|---| --- |
-| url  | string  | -  | false  |  和html属性至少有一个，html属性优先级高    |
-| html  | string  | -  | false  | 要渲染的html代码   |
-| screenshot  | object  | -  | false  |  [参考][3]  |
-| device  | string  | iPhone 8  | false  |  可选值[参考][4]  |
-|  style | string  | -  | false  | [参考][5]   |
-| script  | string  | -  | false  | [参考][6]   |
-|  waitFor | string/number  | -  | false  |  [参考][7]  |
 
+| arguments  | type          | default  | required | desc                                      |
+| ---------- | ------------- | -------- | -------- | ----------------------------------------- |
+| url        | string        | -        | false    | 和 html 属性至少有一个，html 属性优先级高 |
+| html       | string        | -        | false    | 要渲染的 html 代码                        |
+| screenshot | object        | -        | false    | [参考][3]                                 |
+| device     | string        | iPhone 8 | false    | 可选值[参考][4]                           |
+| style      | string        | -        | false    | [参考][5]                                 |
+| script     | string        | -        | false    | [参考][6]                                 |
+| waitFor    | string/number | -        | false    | [参考][7]                                 |
 
-  [1]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagescreenshotoptions
-  [2]: https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
-  [3]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagescreenshotoptions
-  [4]: https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
-  [5]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pageaddstyletagoptions
-  [6]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pageaddscripttagoptions
-  [7]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagewaitforselectororfunctionortimeout-options-args
+[1]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagescreenshotoptions
+[2]: https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
+[3]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagescreenshotoptions
+[4]: https://github.com/GoogleChrome/puppeteer/blob/master/DeviceDescriptors.js
+[5]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pageaddstyletagoptions
+[6]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pageaddscripttagoptions
+[7]: https://zhaoqize.github.io/puppeteer-api-zh_CN/#/class-Page?id=pagewaitforselectororfunctionortimeout-options-args
--- src/controller/index.js
+++ src/controller/index.js
@@ -1,134 +1,138 @@
 /**
  * Created by pengchaoyang on 2018/11/9
  */
-const Screenshot = require('../lib/Screenshot');
-const Boom = require('boom');
-const devices=require('../lib/DeviceDescriptors');
-const deviceNames=devices.map(device=>device.name)
+const Screenshot = require('../lib/Screenshot')
+const Boom = require('boom')
+const devices = require('../lib/DeviceDescriptors')
+const deviceNames = devices.map(device => device.name)
 
-const instance=new Screenshot();
-(async ()=>{
+const instance = new Screenshot()
+;(async () => {
   await instance.launch()
-})();
+})()
 
-async function getMethod (ctx) {
-  let options={
-    url:ctx.query.url,
-    screenshot:{
-      type:ctx.query.type,
-      quality:ctx.query.quality,
-      fullPage:ctx.query.fullPage,
-      clip:{
-        x:ctx.query.x,
-        y:ctx.query.y,
-        width:ctx.query.w,
-        height:ctx.query.h,
+async function getMethod(ctx) {
+  let options = {
+    url: ctx.query.url,
+    screenshot: {
+      type: ctx.query.type,
+      quality: ctx.query.quality,
+      fullPage: ctx.query.fullPage,
+      clip: {
+        x: ctx.query.x,
+        y: ctx.query.y,
+        width: ctx.query.w,
+        height: ctx.query.h
       },
-      omitBackground:ctx.query.o
+      omitBackground: ctx.query.o
     },
-    device:ctx.query.device
+    device: ctx.query.device
   }
-  if(!(options=checkOption(options,ctx))) return
-  ctx.type = options.screenshot.type;
-  try{
-    ctx.body=await instance.getImage(options)
-  }catch (e) {
-    ctx.body=Boom.gatewayTimeout(e.message||'Service Unavailable').output;
-  }
-}
-
-async function postMethod (ctx) {
-  let params=ctx.request.body
-  let options={
-      url:params.url,
-      screenshot:params.screenshot,
-      html:params.html,
-      style:params.style,
-      script:params.script,
-      waitFor:params.waitFor,
-      device:params.device
-  }
-  if(!(options=checkOption(options,ctx))) return
-  ctx.type = options.screenshot.type;
-  try{
-      ctx.body=await instance.getImage(options)
-  }catch (e) {
-      ctx.body=Boom.gatewayTimeout(e.message||'Service Unavailable').output;
+  if (!(options = checkOption(options, ctx))) return
+  ctx.type = options.screenshot.type
+  try {
+    ctx.body = await instance.getImage(options)
+  } catch (e) {
+    ctx.body = Boom.gatewayTimeout(e.message || 'Service Unavailable').output
   }
 }
 
-function checkOption(option,ctx){
-    let screenshot=option.screenshot||{}
-    let style=option.style
-    let script =option.script
-    screenshot.type=screenshot.type?screenshot.type:'png'
-
-    if(!/^https?:\/\/.+/.test(option.url)){
-        ctx.body=Boom.badRequest('invalid url').output;
-        return false
-    }
-    if(screenshot.fullPage){
-        delete screenshot.clip
-    }
-    if(!screenshot.clip){
-        //pass
-    }else if(screenshot.clip&&typeof screenshot.clip!=='object'){
-        ctx.body=Boom.badRequest('invalid screenshot.clip').output;
-        return false
-    }else if(screenshot.clip.x==undefined&&
-        screenshot.clip.y==undefined&&
-        screenshot.clip.width==undefined&&
-        screenshot.clip.height==undefined){
-        delete screenshot.clip
-    }else if(Number.isNaN(Number(screenshot.clip.x))||
-        Number.isNaN(Number(screenshot.clip.y))||
-        Number.isNaN(Number(screenshot.clip.width))||
-        Number.isNaN(Number(screenshot.clip.height))){
-        ctx.body=Boom.badRequest('invalid screenshot.clip').output;
-        return false
-    }else{
-        screenshot.clip.x=Number(screenshot.clip.x)
-        screenshot.clip.y=Number(screenshot.clip.y)
-        screenshot.clip.width=Number(screenshot.clip.width)
-        screenshot.clip.height=Number(screenshot.clip.height)
-    }
-    if(screenshot.type&&!/^jpeg$|^png$/.test(screenshot.type)){
-        ctx.body=Boom.badRequest('invalid type. Optional [jpeg,png]').output;
-        return false
-    }
-    if(screenshot.quality&&!Number.isInteger(screenshot.quality)){
-        ctx.body=Boom.badRequest(`quality is a number`).output;
-        return false
-    }
-    screenshot.omitBackground=!!screenshot.omitBackground
-    if(option.device&&deviceNames.indexOf(option.device)===-1){
-        ctx.body=Boom.badRequest(`invalid device. Optional [${deviceNames}]`).output;
-        return false
-    }else{
-        option.device=devices[option.device||'iPhone 8']
-    }
-    option.screenshot=screenshot
-
-    if(style&&typeof style!=='object'){
-        ctx.body=Boom.badRequest(`style must be an object. {url,content}`).output;
-        return false
-    }
-    if(style&&style.content){
-        delete style.url
-    }
-
-    if(script&&typeof script!=='object'){
-        ctx.body=Boom.badRequest(`script must be an object. {url,content,type}`).output;
-        return false
-    }
-    if(script&&script.content){
-        delete script.url
-    }
-
-    return option
+async function postMethod(ctx) {
+  let params = ctx.request.body
+  let options = {
+    url: params.url,
+    screenshot: params.screenshot,
+    html: params.html,
+    style: params.style,
+    script: params.script,
+    waitFor: params.waitFor,
+    device: params.device
+  }
+  if (!(options = checkOption(options, ctx))) return
+  ctx.type = options.screenshot.type
+  try {
+    ctx.body = await instance.getImage(options)
+  } catch (e) {
+    ctx.body = Boom.gatewayTimeout(e.message || 'Service Unavailable').output
+  }
 }
 
-module.exports={
+function checkOption(option, ctx) {
+  let screenshot = option.screenshot || {}
+  let style = option.style
+  let script = option.script
+  screenshot.type = screenshot.type ? screenshot.type : 'png'
+
+  if (!/^https?:\/\/.+/.test(option.url)) {
+    ctx.body = Boom.badRequest('invalid url').output
+    return false
+  }
+  if (screenshot.fullPage) {
+    delete screenshot.clip
+  }
+  if (!screenshot.clip) {
+    //pass
+  } else if (screenshot.clip && typeof screenshot.clip !== 'object') {
+    ctx.body = Boom.badRequest('invalid screenshot.clip').output
+    return false
+  } else if (
+    screenshot.clip.x == undefined &&
+    screenshot.clip.y == undefined &&
+    screenshot.clip.width == undefined &&
+    screenshot.clip.height == undefined
+  ) {
+    delete screenshot.clip
+  } else if (
+    Number.isNaN(Number(screenshot.clip.x)) ||
+    Number.isNaN(Number(screenshot.clip.y)) ||
+    Number.isNaN(Number(screenshot.clip.width)) ||
+    Number.isNaN(Number(screenshot.clip.height))
+  ) {
+    ctx.body = Boom.badRequest('invalid screenshot.clip').output
+    return false
+  } else {
+    screenshot.clip.x = Number(screenshot.clip.x)
+    screenshot.clip.y = Number(screenshot.clip.y)
+    screenshot.clip.width = Number(screenshot.clip.width)
+    screenshot.clip.height = Number(screenshot.clip.height)
+  }
+  if (screenshot.type && !/^jpeg$|^png$/.test(screenshot.type)) {
+    ctx.body = Boom.badRequest('invalid type. Optional [jpeg,png]').output
+    return false
+  }
+  if (screenshot.quality && !Number.isInteger(screenshot.quality)) {
+    ctx.body = Boom.badRequest(`quality is a number`).output
+    return false
+  }
+  screenshot.omitBackground = !!screenshot.omitBackground
+  if (option.device && deviceNames.indexOf(option.device) === -1) {
+    ctx.body = Boom.badRequest(`invalid device. Optional [${deviceNames}]`).output
+    return false
+  } else {
+    option.device = devices[option.device || 'iPhone 8']
+  }
+  option.screenshot = screenshot
+
+  if (style && typeof style !== 'object') {
+    ctx.body = Boom.badRequest(`style must be an object. {url,content}`).output
+    return false
+  }
+  if (style && style.content) {
+    delete style.url
+  }
+
+  if (script && typeof script !== 'object') {
+    ctx.body = Boom.badRequest(`script must be an object. {url,content,type}`).output
+    return false
+  }
+  if (script && script.content) {
+    delete script.url
+  }
+
+  return option
+}
+
+module.exports = {
   getMethod,
   postMethod
 }
